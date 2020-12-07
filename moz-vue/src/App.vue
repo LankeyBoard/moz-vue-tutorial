281f8526fa389a9d847bc3d98c7729e173ec0879
LankeyBoard <56012902+LankeyBoard@users.noreply.github.com>
2020-12-07 18:17:32 -0500

    completed up to syling vue components with css

--- moz-vue/src/App.vue
+++ moz-vue/src/App.vue
@@ -1,19 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>To-Do List</h1>
+    <to-do-form @todo-added="addToDo"></to-do-form>
+    <ul>
+      <li v-for="item in ToDoItems" :key="item.id">
+        <to-do-item :label="item.label" :done="item.done" :id="item.id"></to-do-item>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoItem from './components/ToDoItem';
+import uniqueId from 'lodash.uniqueid';
+import ToDoForm from './components/ToDoForm.vue';
+
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    ToDoItem,
+    ToDoForm
+  },
+ data() {
+    return {
+      ToDoItems: [
+        { id:uniqueId('todo-'), label: 'Learn Vue', done: false },
+        { id:uniqueId('todo-'), label: 'Create a Vue project with the CLI', done: true },
+        { id:uniqueId('todo-'), label: 'Have fun', done: true },
+        { id:uniqueId('todo-'), label: 'Create a to-do list', done: false }
+      ]
+    };
+  },
+  methods: {
+    addToDo(toDoLabel) {
+      console.log('to-do added: ', toDoLabel);
+      this.ToDoItems.push({id:uniqueId('todo-'), label: toDoLabel, done: false});
+    }
   }
-}
+};
 </script>
 
 <style>
